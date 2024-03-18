3f62d3f5951617c7e697efa9f247b6a10c65b051
Korotaev <sascha564@mail.ru>
2024-03-18 12:36:37 +0300

    rename var start

--- Korotaev_file.py
+++ Korotaev_file.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     maze.generate()
     print("Generated Maze:")
     print(maze)
-    start = (1, 1)
+    start_position = (1, 1)
     end = (width-2, height-2)
     path = maze.solve(start, end)
     print("Solved Maze:")
